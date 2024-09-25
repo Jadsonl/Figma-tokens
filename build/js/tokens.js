cc55565a3239c4cc3cb77827d6e0f2b9bd9a38ae
jadsonl <jadsonunico90@gmail.com>
2024-09-25 15:37:07 -0300

    init

---
+++ build/js/tokens.js
@@ -0,0 +1,17 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+module.exports = {
+  "ColorGray900": "#09090b",
+  "VariableCollectionWhite": "#ffffff",
+  "VariableCollectionBlue500": "#22d3ee",
+  "VariableCollectionGray400": "#18181b",
+  "VariableCollectionGray900": "#09090b",
+  "VariableCollectionR": "#000000",
+  "VariableCollectionColor": "#000000",
+  "VariableCollectionBlack": "#000000",
+  "VariableCollectionZinc400": "#a1a1aa",
+  "VariableCollectionZinc700": "#3f3f46",
+  "VariableCollectionZinc800": "#27272a"
+};
